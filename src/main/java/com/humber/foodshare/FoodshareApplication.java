83ea55999d9cf187ce65cac5a8bc32bdd868166c
sigehiro <sigehiro.0728@gmail.com>
2025-03-18 20:15:00 -0400

    1st commit

---
+++ src/main/java/com/humber/foodshare/FoodshareApplication.java
@@ -0,0 +1,13 @@
+package com.humber.foodshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodshareApplication.class, args);
+	}
+
+}
