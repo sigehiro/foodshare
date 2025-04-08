28366ac563609d1f8abcf2905512982331c2dd97
shevonne221 <abigalesymister@gmail.com>
2025-04-08 19:33:47 -0400

    error check in chat

--- src/main/java/com/humber/foodshare/repositories/ChatMessageRepository.java
+++ src/main/java/com/humber/foodshare/repositories/ChatMessageRepository.java
@@ -1,9 +1,9 @@
-//package com.humber.foodshare.repositories;
-//
-//import com.humber.foodshare.models.ChatMessage;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface  ChatMessageRepository extends MongoRepository<ChatMessage, String> {
-//}
+package com.humber.foodshare.repositories;
+
+import com.humber.foodshare.models.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface  ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+}
