8c88ddd6eb8bbd2dee3148a468c47e769b376b66
shevonne221 <abigalesymister@gmail.com>
2025-04-09 20:44:40 -0400

    Your detailed message about the changes

---
+++ src/main/java/com/humber/foodshare/controllers/ChatPageController.java
@@ -0,0 +1,18 @@
+package com.humber.foodshare.controllers;
+
+import com.humber.foodshare.models.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatPageController {
+
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public ChatMessage send(ChatMessage message) {
+        return message;
+    }
+
+}
